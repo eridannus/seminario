f9d4117c2f0cb83abadf702524c35e5b54e75863
Martín Sánchez <eridannus@gmail.com>
2026-03-11 16:38:59 -0600

    Date format

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,12 +1,12 @@
 import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
 const dateFormat = (
   date: Date | string,
-  pattern: string = "dd MMM, yyyy",
+  pattern: string = "dd MMM yyyy",
 ): string => {
   const dateObj = new Date(date);
-  const output = format(dateObj, pattern);
-  return output;
+  return format(dateObj, pattern, { locale: es });
 };
 
 export default dateFormat;
